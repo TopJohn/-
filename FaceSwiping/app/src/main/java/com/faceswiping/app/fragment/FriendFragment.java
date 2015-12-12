c400d87083bd0ca8dd18a735bd0ef95ca4ab0f26
sjaiwl <761034053@qq.com>
2015-12-13 01:34:06 +0800

    sjaiwl add

--- FaceSwiping/app/src/main/java/com/faceswiping/app/fragment/FriendFragment.java
+++ FaceSwiping/app/src/main/java/com/faceswiping/app/fragment/FriendFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.faceswiping.app.R;
+import com.faceswiping.app.base.BaseFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FriendFragment extends Fragment {
+public class FriendFragment extends BaseFragment {
 
+    @InjectView(R.id.fragment_friend_gridView)
+    GridView gridView;
 
     public FriendFragment() {
         // Required empty public constructor
@@ -24,7 +31,21 @@ public class FriendFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend, container, false);
+        View view = inflater.inflate(R.layout.fragment_friend, container, false);
+        ButterKnife.inject(this, view);
+        initView(view);
+        initData();
+        return view;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void initData() {
+
     }
 
 }
